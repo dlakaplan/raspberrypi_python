69c1862b5409b8e8d03c7afceb426a355022bff3
David Kaplan <kaplan@gravity.phys.uwm.edu>
2014-09-15 14:44:55 -0500

    got it working as an executable

--- update_localinfo.py
+++ update_localinfo.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
+
+import datetime, json, os, sys
+from optparse import OptionParser
+import logging
+
 import mcts
 import wunderground
-import datetime, json
+
 import write_ppm
 
 Stops={'office': {'rt': '30',
@@ -15,14 +21,20 @@ Stops={'office': {'rt': '30',
 
 Station='KWIMILWA39'
 
-class localinfo():    
-    def __init__(self):
-        self.Stops=Stops
-        self.Station=Station
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.WARNING)
+logger = logging.getLogger()
+
+
+##################################################
+class businfo():
+    def __init__(self, stops=Stops):
+        self.Stops=stops
         self.busdata=None
-        self.weatherdata=None
-        
-    def update_stops(self):
+        self.directory='./'
+        self.filename='businfo.data'
+
+    def update(self):
+        logger.info('Updating bus info...')
         self.busdata=None
         for stop in self.Stops.keys():
             if self.busdata is None:
@@ -40,57 +52,39 @@ class localinfo():
                 for bus in temp_busdata['bustime-response']['prd']:
                     self.busdata['bustime-response']['prd'].append(bus)
         self.busdata['querytime']=datetime.datetime.now().strftime('%Y%m%d %H:%M')
-                
-    def update_weather(self):
-    
-        self.weatherdata=wunderground.get_conditions(self.Station,
-                                                     debug=False)
-        self.weatherdata['querytime']=datetime.datetime.now().strftime('%Y%m%d %H:%M')
-
-    def update_all(self):
-        self.update_stops()
-        self.update_weather()
-
         
-    def writetofile(self, filename='localinfo.data'):
+    def writetofile(self):
         try:
-            f=open(filename, 'w')
+            f=open(os.path.join(self.directory, self.filename), 'w')
         except:
-            print 'Error opening %s for writing' % filename
+            logger.error('Error opening %s for writing' % os.path.join(self.directory, self.filename))
         json.dump(self.busdata, f)
-        f.write('\n')
-        json.dump(self.weatherdata, f)
-        f.write('\n')
         f.close()
+        logger.info('Bus info written to %s' % os.path.join(self.directory, self.filename))
 
-    def readfromfile(self, filename='localinfo.data'):
+    def readfromfile(self):
         try:
-            f=open(filename)
+            f=open(os.path.join(self.directory, self.filename))
         except:
-            print 'Error opening %s for reading' % filename
-        lines=f.readlines()
-        f.close()
+            logger.error('Error opening %s for reading' % os.path.join(self.directory, self.filename))
+            return None
         try:
-            self.busdata=json.loads(lines[0])
+            self.busdata=json.load(f)
         except:
-            print 'Unable to read bus data from file %s' % filename
-        try:
-            self.weatherdata=json.loads(lines[1])
-        except:
-            print 'Unable to read weather data from file %s' % filename
-            
+            logger.error('Unable to read bus data from file %s' % os.path.join(self.directory, self.filename))
+            return None
+        logger.info('Bus info read from %s' % os.path.join(self.directory, self.filename))
+        return datetime.datetime.strptime(self.busdata['querytime'],
+                                          '%Y%m%d %H:%M')
+        
         
     def __str__(self):
         now=datetime.datetime.now()
         
         if self.busdata is None:
-            self.update_stops()
-        if self.weatherdata is None:
-            self.update_weather()
+            self.update()
         sout=[]
         sout.append(now.strftime('%a %Y-%m-%d %H:%M'))
-        sout.append('%.1f F' % self.weatherdata['current_observation']['temp_f'])
-        
         for bus in self.busdata['bustime-response']['prd']:
             prdtm=datetime.datetime.strptime(bus['prdtm'],'%Y%m%d %H:%M')
             timefromnow=prdtm-now
@@ -107,10 +101,169 @@ class localinfo():
                                                     round(timefromnow.seconds/60.0),
                                                     sym))
         return '; '.join(sout)
+##################################################
+class weatherinfo():
+    def __init__(self, station=Station):
+        self.Station=station
+        self.weatherdata=None
+        self.directory='./'
+        self.filename='weatherinfo.data'
+
+    def update(self):    
+        logger.info('Updating weather info...')
+        self.weatherdata=wunderground.get_conditions(self.Station,
+                                                     debug=False)
+        self.weatherdata['querytime']=datetime.datetime.now().strftime('%Y%m%d %H:%M')
+
+
+    def writetofile(self):
+        try:
+            f=open(os.path.join(self.directory, self.filename),'w')
+        except:
+            logger.error('Error opening %s for writing' % os.path.join(self.directory, self.filename))
+        json.dump(self.weatherdata, f)
+        f.close()
+        logger.info('Weather info written to %s' % os.path.join(self.directory, self.filename))
+
+    def readfromfile(self):
+        try:
+            f=open(os.path.join(self.directory, self.filename))
+        except:
+            logger.error('Error opening %s for reading' % os.path.join(self.directory, self.filename))
+            return None
+        try:
+            self.weatherdata=json.load(f)
+        except:
+            logger.error('Unable to read weather data from file %s' % os.path.join(self.directory, self.filename))
+            return None
+        logger.info('Weather info read from %s' % os.path.join(self.directory, self.filename))
+        return datetime.datetime.strptime(self.weatherdata['querytime'],
+                                          '%Y%m%d %H:%M')
+                
+    def __str__(self):
+        now=datetime.datetime.now()
+        
+        if self.weatherdata is None:
+            self.update()
+        sout=[]
+        sout.append(now.strftime('%a %Y-%m-%d %H:%M'))
+        sout.append('%.1f F' % self.weatherdata['current_observation']['temp_f'])
+        return '; '.join(sout)
+
+        
+##################################################
+class localinfo():    
+    def __init__(self, directory=None):
+        self.Stops=Stops
+        self.Station=Station
+        if directory is None:
+            self.directory='./'
+        else:
+            self.directory=directory
+        self.businfo=businfo(stops=self.Stops)
+        self.weatherinfo=weatherinfo(station=self.Station)
+        self.businfo.directory=self.directory
+        self.weatherinfo.directory=self.directory
+        
+    def update_stops(self):
+        self.businfo.update()
+        
+    def update_weather(self):
+        self.weatherinfo.update()
+
+    def update(self):
+        self.update_stops()
+        self.update_weather()
+        
+    def writetofile(self):
+        self.businfo.writetofile()
+        self.weatherinfo.writetofile()
+
+    def readfromfile(self):
+        result=self.businfo.readfromfile()
+        if result is None:
+            logger.warning('Could not read bus info from file; updating from net...')
+            self.update_stops()
+            self.businfo.writetofile()
+        result=self.weatherinfo.readfromfile()
+        if result is None:
+            logger.warning('Could not read weather info from file; updating from net...')
+            self.update_weather()
+            self.weatherinfo.writetofile()            
+        
+    def __str__(self):
+        now=datetime.datetime.now()
+        
+        if self.businfo.busdata is None:
+            logger.warning('Bus info not loaded; updating from net...')
+            self.update_stops()
+        if self.weatherinfo.weatherdata is None:
+            logger.warning('Weather info not loaded; updating from net...')
+            self.update_weather()
+        sout=[]
+        soutbus=str(self.businfo)
+        soutweather=str(self.weatherinfo)
+        
+        sout.append(now.strftime('%a %Y-%m-%d %H:%M'))
+        sout+=soutweather.split('; ')[1:]
+        sout+=soutbus.split('; ')[1:]
+
+        return '; '.join(sout)
     
         
     def writeimage(self, filename='localinfo.ppm'):
         sout=str(self)
         t=write_ppm.textppm(sout)
-        t.write(filename)
+        t.write(os.path.join(self.directory, filename))
+        logger.info('Local info written to %s' % os.path.join(self.directory, filename))
         
+##################################################
+def main():
+    usage="Usage: %prog [options]\n"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-d','--directory',dest='directory',default='./',
+                      help='Output directory [default=%default]')
+    parser.add_option('-u','--update',dest='update',default='none',
+                      type='choice',
+                      choices=['none','all','bus','weather'],
+                      help='Which info (if any) to update [default=%default]')
+    parser.add_option('-r','--read',dest='read',default=False,
+                      action='store_true',
+                      help='Read data from local files (instead of updating?)')
+    parser.add_option('-w','--write',dest='write',default=False,
+                      action='store_true',
+                      help='Write PPM image?')
+    parser.add_option('-i','--image', dest='image', default='localinfo.ppm',
+                      help='Output PPM image name [default=%default]')
+    parser.add_option('-v','--verbose', dest='verbose',default=False,
+                      action='store_true',
+                      help='Increase verbosity of output?')
+
+
+    (options, args) = parser.parse_args()
+    if options.verbose:
+        logger.setLevel(logging.INFO)
+
+    l=localinfo()
+    l.directory=options.directory
+    if options.update == 'bus':
+        l.update_stops()
+        self.businfo.writetofile()
+    elif options.update == 'weather':
+        l.update_weather()
+        self.weatherinfo.writetofile()            
+    elif options.update == 'all':
+        l.update()
+        l.writetofile()
+
+    if options.read:
+        l.readfromfile()
+
+    if options.write:
+        l.writeimage(filename=options.image)
+        
+
+######################################################################
+# Running as executable
+if __name__=='__main__':
+    main()
